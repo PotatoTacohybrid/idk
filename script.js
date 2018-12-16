7f05d5d3e3453ffcae8f753c0b9439af98a68479
Tommy Wisaeu <kingdomkeygaming@gmail.com>
2018-12-16 13:58:37 -0600

    Added style.css and updated hello.html and scriptjs

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-  alert("hello user")
-})
+
+  function writeToLog(text, type) {
+    $("#log").html("<p class=" + type + ">" + text + "</p>")
+  }
+
+  $("#button").click(function() {
+    var  variable = $("#input").val();
+
+    if (!variable) {
+      writeToLog("Please enter fields", "alert")
+    }
+    else {
+      writeToLog("Hello, " + variable, "success")
+    }
+  });
+});
